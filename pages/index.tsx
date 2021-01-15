f7411d9f6c7a08923dffb1e84118b306db5023d7
Rushi <rushi44@gmail.com>
2021-01-14 20:35:57 -0800

    create home page + custom icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,35 @@
 import Head from 'next/head'
-import { Button, Code, Heading, Text } from '@chakra-ui/react'
+import { Button,  Flex } from '@chakra-ui/react'
 
 import { useAuth } from '@lib/auth'
+import { LogoIcon } from '@styles/Icons/LogoIcon'
 
 export default function Home() {
   const auth = useAuth()
-  
+
   return (
-    <div>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Feedback Loop</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Heading>
-          Welcome to <a href="https://nextjs.org">Feedback Loop</a>
-        </Heading>
-
-        <Text>
-          Current User: <Code>{auth?.user?.email || 'None'}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={() => auth.signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => auth.signInWithGitHub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      <LogoIcon boxSize={16} />
+      {auth.user ? (
+        <Button onClick={() => auth.signOut()}>Sign Out</Button>
+      ) : (
+        <Button
+          mt={4}
+          size="sm"
+          onClick={() => auth.signInWithGitHub()}
+        >
+          Sign In
+        </Button>
+      )}
+    </Flex>
   )
 }
