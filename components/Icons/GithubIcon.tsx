95a3845dcad423077ada5ee2a22b8e5d6fe8ea47
Rushi <rushi44@gmail.com>
2021-01-18 13:32:52 -0800

    create custom icons with react-icons :heavy_plus_sign:

---
+++ components/Icons/GithubIcon.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@chakra-ui/react'
+import { FiGithub } from 'react-icons/fi'
+
+export const GithubIcon = (props: any) => <Icon as={FiGithub} {...props} />
---
+++ components/Icons/GoogleIcon.tsx
@@ -0,0 +1,4 @@
+import { Icon } from '@chakra-ui/react'
+import { FcGoogle } from 'react-icons/fc'
+
+export const GoogleIcon = (props: any) => <Icon as={FcGoogle} {...props} />
