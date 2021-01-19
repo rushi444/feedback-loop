2794a4eed4b8d39a51a16564423b41234b1382d7
Rushi <rushi44@gmail.com>
2021-01-18 20:01:36 -0800

    set up logger :rocket:

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { auth } from '@lib/firebase-admin'
 import { getFeedbackByUser } from '@lib/db-admin'
+import { formatObjectKeys, logger } from '@utils/logger'
 
 const fetchFeedbackByUser = async (
   req: NextApiRequest,
@@ -14,6 +15,19 @@ const fetchFeedbackByUser = async (
     const feedback = await getFeedbackByUser(uid)
     res.status(200).json(feedback)
   } catch (err) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      err.message
+    )
     res.status(500).json(err)
   }
 }
--- pages/api/sites.ts
+++ pages/api/sites.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { auth } from '@lib/firebase-admin'
-import {  getAllSitesByUser } from '@lib/db-admin'
+import { getAllSitesByUser } from '@lib/db-admin'
+import { formatObjectKeys, logger } from '@utils/logger'
 
 const fetchAllSites = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -11,6 +12,19 @@ const fetchAllSites = async (req: NextApiRequest, res: NextApiResponse) => {
     const sites = await getAllSitesByUser(uid)
     res.status(200).json(sites)
   } catch (err) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      err.message
+    )
     res.status(500).json(err)
   }
 }
